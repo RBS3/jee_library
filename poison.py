33c581194eab4504900c60990e48e32c7f66e4d8
RBS <144377785+RBS3@users.noreply.github.com>
2026-04-01 20:17:30 +0100

    Delete poison.py

--- poison.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def get_user_data(username):
-    db = sqlite3.connect("database.db")
-    cursor = db.cursor()
-    # POISON: Concatenating strings in a query is a massive vulnerability
-    query = "SELECT * FROM users WHERE username = '" + username + "'"
-    cursor.execute(query)
-    return cursor.fetchall()
